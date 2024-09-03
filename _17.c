7db9dcbdd2996e01b2aa9edede908b65d7b42a9f
Raunak4518 <u24ai109@aid.svnit.ac.in>
2024-09-04 02:46:01 +0530

    Add files via upload

---
+++ _17.c
@@ -0,0 +1,15 @@
+// PROGRAM TO READ THREE NOS. AND PRINT MAX USING LOGICAL OPERATOR.(&&)
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter three numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c)
+    {printf(" max no is %d",a);}
+    else
+    { if(b>a && b>c)
+    {printf("max number is %d",b);}
+    else {printf(" max number is %d",c);}
+    }
+    return 0;
+}
